b4dd81e3a6e6d008ccd173addac0de16e547f651
Vincenzo Pipitone <82387646+pipitonedev@users.noreply.github.com>
2021-11-23 10:14:04 +0100

    Creazione progetto

---
+++ src/main/java/it/prova/gestionetriage/GestionetriageApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionetriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionetriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionetriageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/gestionetriage/GestionetriageApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionetriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionetriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
