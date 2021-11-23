5f47652b9b1f7e666e7fbacb8d801dae26d4f8d7
Vincenzo Pipitone <82387646+pipitonedev@users.noreply.github.com>
2021-11-23 14:20:42 +0100

    Implementati jwt per la sicurezza

---
+++ src/main/java/it/prova/gestionetriage/security/jwt/JwtAuthenticationRequest.java
@@ -0,0 +1,35 @@
+package it.prova.gestionetriage.security.jwt;
+
+import javax.validation.constraints.NotBlank;
+
+public class JwtAuthenticationRequest {
+	@NotBlank
+	private String username;
+	@NotBlank
+	private String password;
+
+	public JwtAuthenticationRequest() {
+		super();
+	}
+
+	public JwtAuthenticationRequest(String username, String password) {
+		this.setUsername(username);
+		this.setPassword(password);
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/it/prova/gestionetriage/security/jwt/JwtTokenUtil.java
@@ -0,0 +1,61 @@
+package it.prova.gestionetriage.security.jwt;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+
+@Component
+public class JwtTokenUtil {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenUtil.class);
+
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
+	@Value("${jwt.expiration}")
+	private Long jwtExpirationMs;
+
+	public String generateJwtToken(Authentication authentication) {
+
+		JwtUserDetailsImpl userPrincipal = (JwtUserDetailsImpl) authentication.getPrincipal();
+
+		return Jwts.builder().setSubject((userPrincipal.getUsername())).setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+				.signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+	}
+
+	public String getUserNameFromJwtToken(String token) {
+		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+	}
+
+	public boolean validateJwtToken(String authToken) {
+		try {
+			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+			return true;
+		} catch (SignatureException e) {
+			LOGGER.error("Invalid JWT signature: {}", e.getMessage());
+		} catch (MalformedJwtException e) {
+			LOGGER.error("Invalid JWT token: {}", e.getMessage());
+		} catch (ExpiredJwtException e) {
+			LOGGER.error("JWT token is expired: {}", e.getMessage());
+		} catch (UnsupportedJwtException e) {
+			LOGGER.error("JWT token is unsupported: {}", e.getMessage());
+		} catch (IllegalArgumentException e) {
+			LOGGER.error("JWT claims string is empty: {}", e.getMessage());
+		}
+
+		return false;
+	}
+}
